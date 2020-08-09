85cb0c25009a90a048857ddaddb971e2adcf75c3
Fajar Muhammad Fathirochman <fajarmuhf@gmail.com>
2020-08-09 20:02:51 +0700

    Flappy Bird Init
    Build on Mac and Source Code of Flappy BIrd

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Komponen akan ditambahkan jika tidak ada dan komponen tersebut tidak dapat dibuang
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed = 8;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Mencari koponent pipa dari object yang masuk ke area trigger
+        Pipe pipa = collision.GetComponent<Pipe>();
+        //Pengecekan null variable
+        if (pipa)
+        {
+            //Memusnahkan object pipa ketika bersentuhan
+            Destroy(collision.gameObject);
+            //Memusnahkan object peluru ketika bersentuhan
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/BulletSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Komponen akan ditambahkan jika tidak ada dan komponen tersebut tidak dapat dibuang
+[RequireComponent(typeof(BoxCollider2D))]
+public class BulletSpawn : MonoBehaviour
+{
+    //Global variables
+    [SerializeField] private Bird bird;
+    [SerializeField] private Bullet bullet;
+    
+    //Dipanggil setiap frame
+    private void Update()
+    {
+        //Melakukan pengecekan jika pemain menekan space dikeyboard
+        if (Input.GetKeyDown("space"))
+        {
+            //menduplikasi game object Bullet dan menempatkan posisinya sama dengan Bird game object
+            Bullet newBullet = Instantiate(bullet, bird.getPosition(), Quaternion.identity);
+            //Mengaktifkan game object Bullet
+            newBullet.gameObject.SetActive(true);
+
+        }
+    }
+
+}
